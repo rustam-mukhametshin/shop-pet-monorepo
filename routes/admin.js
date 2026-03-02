c640af3a31267c2d366c9ff10ff793d4b1dedd7c
EXTERNAL Mukhametshin Rustam (EPAM, BD/CSA-PUR) <external.Rustam.Mukhametshin@de.bosch.com>
2026-03-02 14:43:06 +0500

    add rootdir

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../utils/path');
 
 const adminRoutes = express.Router();
 
 
 adminRoutes.get('/add-product', (req, res, next) => {
-    return res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    return res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 adminRoutes.post('/product', (req, res, next) => {
