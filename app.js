4fb8a941f1919be97ec845fcea51736af6a764b2
EXTERNAL Mukhametshin Rustam (EPAM, BD/CSA-PUR) <external.Rustam.Mukhametshin@de.bosch.com>
2026-03-04 19:55:39 +0500

    use ejs view engine

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+
+app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
 // Start public
 app.use(express.static(path.join(__dirname, 'public')))
 // End public
