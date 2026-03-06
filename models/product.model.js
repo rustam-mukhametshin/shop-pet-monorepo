e5826d1032d4a27cba3241faec4db80c944f071a
EXTERNAL Mukhametshin Rustam (EPAM, BD/CSA-PUR) <external.Rustam.Mukhametshin@de.bosch.com>
2026-03-06 21:02:31 +0500

    refactor products

--- models/product.model.js
+++ models/product.model.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        return cb(JSON.parse(fileContent));
+    });
+}
+
 module.exports = class Product {
     constructor(
         title = '',
@@ -9,14 +24,8 @@ module.exports = class Product {
     }
 
     save() {
-        const p = Product.#getFilePath();
-        fs.readFile(p, (err, fileContent) => {
-            debugger
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent) || [];
-                products.push(this);
-            }
+        getProductsFromFile(products => {
+            products.push(this);
             fs.writeFile(p, JSON.stringify(products), e => {
                 console.log(e);
             });
@@ -24,19 +33,6 @@ module.exports = class Product {
     }
 
     static getAll(cb) {
-        fs.readFile(this.#getFilePath(), (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
-    }
-
-    static #getFilePath() {
-        return path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        )
+        return getProductsFromFile(cb);
     }
 }
