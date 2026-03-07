18096b88c037d993de18c08cd69e3e2851a6ac4c
EXTERNAL Mukhametshin Rustam (EPAM, BD/CSA-PUR) <external.Rustam.Mukhametshin@de.bosch.com>
2026-03-07 19:43:13 +0500

    update routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const ProductController = require("../controllers/product.controller");
+const ShopController = require("../controllers/shop.controller");
 const shopRoutes = express.Router();
 
 
-shopRoutes.get('/products', ProductController.getProducts)
-shopRoutes.get('/', ProductController.getHomepage)
+shopRoutes.get('/products', ShopController.getProducts)
+shopRoutes.get('/cart', ShopController.getHomepage)
+shopRoutes.get('/checkout', ShopController.getHomepage)
+shopRoutes.get('/', ShopController.getHomepage)
 
 module.exports = shopRoutes;
