b74882f3c00fadb33dc4bfcfd47b79c1e350d9ad
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-03-23 18:36:06 +0500

    add sequelize

--- util/database.js
+++ util/database.js
@@ -1,12 +1,13 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || '',
-    database: process.env.DB_NAME || '',
-    password: process.env.DB_PASSWORD || '',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME || '',
+    process.env.DB_USER || '',
+    process.env.DB_PASSWORD || '',
+    {
+        dialect: 'mysql',
+        host: process.env.DB_HOST || 'localhost'
+    }
+)
 
-module.exports = pool.promise();
-
-
+module.exports = sequelize;
