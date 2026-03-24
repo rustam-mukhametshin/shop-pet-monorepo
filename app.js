fe6e5ab84cef1682f6e7cf6a86f7e0a23b1f665e
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-03-24 19:38:25 +0500

    add relations

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const adminRoutes = require('./routes/admin.routes');
 const shopRoutes = require('./routes/shop.routes');
 const {notFound} = require("./controllers/public.controller");
 const sequelize = require('./util/database');
+const Product = require('./models/product.model');
+const User = require('./models/user.model');
 
 const app = express();
 
@@ -26,6 +28,13 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(notFound)
+
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE',
+});
+User.hasMany(Product);
+
 
 sequelize.sync()
     .then(() => app.listen(3333))
