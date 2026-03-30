b860a6e5c8866d5a7b2911a93c19473ee66cb58c
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-03-30 16:31:27 +0500

    change to mongodb

--- util/database.ts
+++ util/database.ts
@@ -1,14 +1,39 @@
-import { Sequelize } from 'sequelize';
+import {Db, MongoClient} from "mongodb";
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME || '',
-    process.env.DB_USER || '',
-    process.env.DB_PASSWORD || '',
-    {
-        dialect: 'mysql',
-        host: process.env.DB_HOST || 'localhost',
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME || '',
+//     process.env.DB_USER || '',
+//     process.env.DB_PASSWORD || '',
+//     {
+//         dialect: 'mysql',
+//         host: process.env.DB_HOST || 'localhost',
+//     }
+// );
+//
+// export default sequelize;
+
+let _db: Db;
+
+export const mongoConnect = (cb: (result: unknown) => {}) => {
+    MongoClient.connect(
+        process.env.MONGO_URI || 'mongodb+srv://:@cluster0.japfcdr.mongodb.net/?appName=Cluster0',
+        {})
+        .then(result => {
+            console.info("Database connected successfully.");
+            _db = result.db('shop');
+            cb(result);
+        })
+        .catch((err) => {
+            console.error(err);
+            throw err;
+        })
+}
+
+export const getDb = () => {
+    if (_db) {
+        return _db;
     }
-);
-
-export default sequelize;
+    throw new Error('MongoDB connection failed!');
+}
 
