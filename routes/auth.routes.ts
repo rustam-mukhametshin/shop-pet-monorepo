f7480ec739ad75eec35a5a3fd1f7e58d16e4f2aa
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-04-13 17:43:22 +0500

    add post for login

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -4,6 +4,7 @@ import * as AuthController from '../controllers/auth.controller';
 const authRoutes = Router();
 
 authRoutes.get('/login', AuthController.getLogin);
+authRoutes.post('/login', AuthController.postLogin);
 
 export default authRoutes;
 
