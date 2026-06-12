8646245afcefa82f25ebfefc474983e050b31129
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-06-12 11:45:13 +0500

    fix id

--- controllers/admin.controller.ts
+++ controllers/admin.controller.ts
@@ -64,7 +64,7 @@ export const postAddProduct = (req: Request, res: Response, next: any): any => {
 
 export const getProducts = (req: Request, res: Response): Promise<void> => {
     return Product.find({
-        userId: new ObjectId(req.user.id),
+        userId: new ObjectId(req?.user?.id),
     })
         .populate('userId', 'name')
         .then((products) => {
