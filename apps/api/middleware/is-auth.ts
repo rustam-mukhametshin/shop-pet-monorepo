db436be886036f1acd06482111e215780baa3503
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-06-15 20:33:09 +0500

    update isAuth middleware

--- apps/api/middleware/is-auth.ts
+++ apps/api/middleware/is-auth.ts
@@ -1,8 +1,34 @@
-import {NextFunction} from "express";
+import {NextFunction, Request, Response} from "express";
+import jwt from 'jsonwebtoken';
 
-export function isAuth(req: any, res: any, next: NextFunction): void {
-    if (!req.session.isLoggedIn) {
-        return res.redirect('/login');
+export function isAuth(req: Request, res: Response, next: NextFunction) {
+    const token = req.get('Authorization')?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: 'Not logged in',
+        });
     }
-    next();
+
+    if (!process.env.JWT_SECRET) {
+        return res.status(500).json({
+            success: false,
+            message: 'Server configuration error',
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) as any;
+        if (decoded?.userId) {
+            req.userId = decoded.userId;
+            return next();
+        }
+        throw new Error('Unauthorized');
+    } catch (err) {
+        return res.status(401).json({
+            success: false,
+            message: 'Invalid or expired token',
+        });
+    }
 }
