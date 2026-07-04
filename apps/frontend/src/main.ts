d08aeb0c0f3d011068499515f36c62df1bb5e631
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-07-04 19:12:23 +0500

    [fe] add animations

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -3,11 +3,13 @@ import {AppComponent} from "./app/app.component";
 import {provideHttpClient} from "@angular/common/http";
 import {provideRouter, withViewTransitions} from "@angular/router";
 import {appRoutes} from "./app/app.routes";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(appRoutes, withViewTransitions()),
+    provideAnimationsAsync()
   ]
 })
   .catch(err => console.error(err));
