9b502e75b7af192a6e533454e1bf2707dc1be08f
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-07-04 19:05:34 +0500

    [fe] add transitions

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,13 +1,13 @@
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
 import {provideHttpClient} from "@angular/common/http";
-import {provideRouter} from "@angular/router";
+import {provideRouter, withViewTransitions} from "@angular/router";
 import {appRoutes} from "./app/app.routes";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withViewTransitions()),
   ]
 })
   .catch(err => console.error(err));
