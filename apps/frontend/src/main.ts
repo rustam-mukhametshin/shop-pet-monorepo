447916fe97d18e6f54370ec4f2909849bcf6d3b0
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-07-11 17:21:13 +0500

    [fe] add zoneless change detection

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -4,9 +4,11 @@ import {provideHttpClient} from "@angular/common/http";
 import {provideRouter, withViewTransitions} from "@angular/router";
 import {appRoutes} from "./app/app.routes";
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import {provideExperimentalZonelessChangeDetection} from "@angular/core";
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideExperimentalZonelessChangeDetection(),
     provideHttpClient(),
     provideRouter(appRoutes, withViewTransitions()),
     provideAnimationsAsync()
