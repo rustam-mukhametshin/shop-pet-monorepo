26be0ba869263f7822c7d2d7f2e4117638c36d1e
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-07-21 21:07:31 +0500

    [fe] fix tests

--- apps/frontend/src/app/products/view-product/view-product.component.spec.ts
+++ apps/frontend/src/app/products/view-product/view-product.component.spec.ts
@@ -1,10 +1,15 @@
 import {ViewProductComponent} from "./view-product.component";
+import {ActivatedRoute} from "@angular/router";
+import {ProductsService} from "../products.service";
 
 describe("ViewProductComponent", () => {
   let component: ViewProductComponent;
 
   beforeEach(async () => {
-    component = new ViewProductComponent();
+    component = new ViewProductComponent(
+      new ActivatedRoute(),
+      new ProductsService(null as any),
+    );
   });
 
   it("should create", () => {
