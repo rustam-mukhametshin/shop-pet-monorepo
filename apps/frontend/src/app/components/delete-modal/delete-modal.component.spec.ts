fb879a374d02d3c3baeed3367ed6fb10407c13ad
Rustam Mukhametshin <rustam404amarao@gmail.com>
2026-07-21 16:00:57 +0500

    [fe] add empty delete modal

---
+++ apps/frontend/src/app/components/delete-modal/delete-modal.component.spec.ts
@@ -0,0 +1,13 @@
+import {DeleteModalComponent} from './delete-modal.component';
+
+describe('DeleteModalComponent', () => {
+  let component: DeleteModalComponent;
+
+  beforeEach(async () => {
+    component = new DeleteModalComponent();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
